2ed2d077906f1eea73cb14ec2d0441bd1076c3bd
cree-per-com <kcossld-naver.com>
2024-03-12 15:24:15 +0900

    ㅇㅇ

--- src/main/java/com/example/postmanex/Main.java
+++ src/main/java/com/example/postmanex/Main.java
@@ -1,2 +1,15 @@
-package com.example.postmanex;public class Main {
+package com.example.postmanex;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Main {
+    @GetMapping("/main")
+    @ResponseBody
+    public String main(@RequestParam("name") String name) {
+        return "안녕하세요 "+ name+ " 님 !!";
+    }
 }
